5f6ef2e2760930b2de88e8f06dad15841b97f662
Erhan Gündüz <erhangndz@live.com>
2023-09-03 14:55:26 +0300

    upload file with api and apiconsume

---
+++ RapidAPI/Controllers/FileController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace RapidAPI.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+            var bytes= stream.ToArray();
+
+            ByteArrayContent byteArrayContent = new ByteArrayContent(bytes);
+            byteArrayContent.Headers.ContentType= new MediaTypeHeaderValue(file.ContentType);
+            MultipartFormDataContent multipart= new MultipartFormDataContent();
+            multipart.Add(byteArrayContent, "file", file.FileName);
+            var httpclient = new HttpClient();
+            await httpclient.PostAsync("http://localhost:38080/api/FileImage", multipart);
+           
+            return View();
+        }
+    }
+}
