d76c531f603f5fb7a06b5feb594a01a30ea98722
Erhan Gündüz <erhangndz@live.com>
2023-12-05 22:49:22 +0300

    weather forecast api added and nested json values

---
+++ RapidAPI/Controllers/WeatherController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidAPI.Models;
+
+namespace RapidAPI.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+           
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://yahoo-weather5.p.rapidapi.com/weather?location=ankara&format=json&u=c"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "e782ab3024msh2e78af442950fd0p115136jsnc3dd0b8c5f54" },
+        { "X-RapidAPI-Host", "yahoo-weather5.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<WeatherViewModel>(body);
+               return View(values);
+            }
+        }
+    }
+}
---
+++ RapidAPI/Models/WeatherViewModel.cs
@@ -0,0 +1,69 @@
+﻿namespace RapidAPI.Models
+{
+    public class WeatherViewModel
+    {
+
+        
+            public Location location { get; set; }
+            public Current_Observation current_observation { get; set; }
+            public Forecast[] forecasts { get; set; }
+        
+
+        public class Location
+        {
+            public string city { get; set; }
+            public int woeid { get; set; }
+            public string country { get; set; }
+            public float lat { get; set; }
+            public float _long { get; set; }
+            public string timezone_id { get; set; }
+        }
+
+        public class Current_Observation
+        {
+            public int pubDate { get; set; }
+            public Wind wind { get; set; }
+            public Atmosphere atmosphere { get; set; }
+            public Astronomy astronomy { get; set; }
+            public Condition condition { get; set; }
+        }
+
+        public class Wind
+        {
+            public int chill { get; set; }
+            public string direction { get; set; }
+            public int speed { get; set; }
+        }
+
+        public class Atmosphere
+        {
+            public int humidity { get; set; }
+            public float visibility { get; set; }
+            public float pressure { get; set; }
+        }
+
+        public class Astronomy
+        {
+            public string sunrise { get; set; }
+            public string sunset { get; set; }
+        }
+
+        public class Condition
+        {
+            public int temperature { get; set; }
+            public string text { get; set; }
+            public int code { get; set; }
+        }
+
+        public class Forecast
+        {
+            public string day { get; set; }
+            public int date { get; set; }
+            public int high { get; set; }
+            public int low { get; set; }
+            public string text { get; set; }
+            public int code { get; set; }
+        }
+
+    }
+}
